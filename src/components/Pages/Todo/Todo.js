6e012c59b92ef9acc959fd5b3372c08f5b31ece8
Franciscogz <65878436+Franciscogz@users.noreply.github.com>
2021-03-15 22:45:23 -0600

    Tarea

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -9,45 +9,32 @@ import firebaseSDK from '../../../FireBaseInit';
 function Todo(){
   const [todoData, setTodoData] = useState({
     todos:[],
-    newTodo:"",
+    newTodo:""
   });
 
   useEffect(
     ()=>{
       const todosRef = firebaseSDK.database().ref('todos').orderByKey().limitToLast(100);
-      todosRef.on('child_added', (snapshot) => {
-        let newTodo = { ...snapshot.val(), fb_id: snapshot.key };
-        let newTodos = todoData.todos;
-        newTodos.push(newTodo);
+      todosRef.on('value', (snapshot)=>{
+        const fb_todos = snapshot.val();
+        const newTodos = [];
+        for (let fb_id in fb_todos) {
+          newTodos.push({...fb_todos[fb_id], fb_id});
+        }
         setTodoData({...todoData, todos: newTodos});
-      })
+
+      });
+      return ()=>{
+        console.log("UnMounting");
+        todosRef.off();
+      }
     },
+
     []
   );
-  todosRef.on('child_removed', (snapshot)=>{
-    const deletedKey = snapshot.key;
-    let newTodos = todoData.todos.filter(o=>{
-      return o.fb_id !==deletedKey;
-    });
-    setTodoData({ ...todoData, todos: newTodos });
-  });
-  todosRef.on('child_changed', (snapshot) => {
-    const changedKey = snapshot.key;
-    const data = snapshot.val();
-    let newTodos = todoData.todos.map(o => {
-      if (o.fb_id == changedKey) {
-        o = {...o, ...data};
-      }
-      return o;
-    });
-    setTodoData({ ...todoData, todos: newTodos });
-  });
-  return ()=>{
-    todosRef.off();
-  }
 
   const onChange = (e)=>{
-    const {name, value} = e.currentTarget;
+    const {value} = e.currentTarget;
     setTodoData({...todoData, newTodo: value});
   };
   const onAddNew = (e)=>{
@@ -56,31 +43,26 @@ function Todo(){
       completed:false,
       id : new Date().getTime()
     };
-
     firebaseSDK.database().ref("todos").push(newToo);
-   // let newTodos = todoData.todos;
-    //newTodos.push(newToo);
-
-    setTodoData({todos:newTodos, newTodo: ""});
   }
   const doneHandler = (id)=>{
-    const newTodos = todoData.todos.map((o)=>{
-      if(o.id == id){
-        o.completed = !o.completed;
-      }
-      return o;
+    const ref = firebaseSDK.database().ref("todos")
+    const fbTodo = ref.child(id);
+    const lcTodo = todoData.todos.find( (o)=>{
+      return o.fb_id === id;
+    });
+    fbTodo.update({
+      "completed": !lcTodo.completed
     });
 
-    setTodoData({...todoData, todos:newTodos});
   };
   const deleteHandler = (id)=>{
-    const newTodos = todoData.todos.filter((o) => {
-      return o.id !==id;
-    });
-
-    setTodoData({ ...todoData, todos: newTodos });
+    if (!todoData.waiting){
+      const ref = firebaseSDK.database().ref("todos")
+      const fbTodo = ref.child(id);
+      fbTodo.remove();
+    }
   }
-  const tmpTodos = todoData.todos.map( (o)=>{return JSON.stringify(o)} ).join(" | ");
   return (
     <section className=" w-full mx-1 lg:w-1/3 lg:mx-auto lg:my-40 mt-20 border-8 border-double border-gray-200 bg-yellow-700 bg-opacity-40">
      <NewTodo
@@ -97,5 +79,4 @@ function Todo(){
     </section>
   )
 }
-
 export default Todo;
--- src/components/Pages/Todo/TodoList.js
+++ src/components/Pages/Todo/TodoList.js
@@ -1,13 +1,11 @@
 import { MdDone, MdDelete} from 'react-icons/md';
 
-// ()=>     fat arrow functions  funciones de flecha gorda ;
-
-function TodoListItem({descripcion, id, completed, doneHandler, deleteHandler}){
+function TodoListItem({descripcion, id, fb_id, completed, doneHandler, deleteHandler}){
   const onClick = (e)=>{
-    doneHandler(id);
+    doneHandler(fb_id);
   }
   const onDeleteClick = (e)=>{
-    deleteHandler(id);
+    deleteHandler(fb_id);
   }
 
   const myClass = (completed)?"striked": "";
@@ -27,12 +25,13 @@ function TodoList({todos, doneHandler, deleteHandler}){
   const todoItems = todos.map((o)=>{
     return (
       <TodoListItem
-        key={o.doneHandlerid}
-        descripcion={o.description}
-        id={o.id}
-        completed={o.completed}
-        doneHandler={doneHandler}
-        deleteHandler={deleteHandler}
+      key={o.id}
+      descripcion={o.description}
+      id={o.id}
+      fb_id={o.fb_id}
+      completed={o.completed}
+      doneHandler={doneHandler}
+      deleteHandler={deleteHandler}
       />
     )
   });
